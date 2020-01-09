83bc12730ccef10c5676e013d3f9750bbda085f4
Vincent Tran <VincentTran197120@gmail.com>
2020-01-09 14:00:51 -0500

    Added front end

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OverallStatusComponent } from './pages/overall-status/overall-status.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OverallStatusComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/pages/overall-status/overall-status.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-overall-status',
+  templateUrl: './overall-status.component.html',
+  styleUrls: ['./overall-status.component.scss']
+})
+export class OverallStatusComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
