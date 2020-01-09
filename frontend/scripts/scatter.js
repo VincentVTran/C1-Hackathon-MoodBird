6c8d88c844e67265daf0b080f16ed93fc6860e03
Meico Hsiao <meicohsiao@gmail.com>
2020-01-09 16:38:40 -0500

    update

---
+++ frontend/scripts/scatter.js
@@ -0,0 +1,27 @@
+var Chart = require('chart.js');
+var scatterChart = new Chart(ctx, {
+    type: 'scatter',
+    data: {
+        datasets: [{
+            label: 'Scatter Dataset',
+            data: [{
+                x: -10,
+                y: 0
+            }, {
+                x: 0,
+                y: 10
+            }, {
+                x: 10,
+                y: 5
+            }]
+        }]
+    },
+    options: {
+        scales: {
+            xAxes: [{
+                type: 'linear',
+                position: 'bottom'
+            }]
+        }
+    }
+});
