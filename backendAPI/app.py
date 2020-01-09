f2f7fe62860e5476ebb8a1d9eb6c7eda6318b0a1
Sahir Mody <sahir.mody@gmail.com>
2020-01-09 13:50:54 -0500

    add API route

--- backendAPI/app.py
+++ backendAPI/app.py
@@ -6,21 +6,26 @@ import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from flask import Flask, render_template, url_for, request, redirect, session
 
-
 app = Flask(__name__)
 
+# URL = 
 
-# Start page for the app where users can enter the stock symbol of
-# the company that they would like to analyze
-@app.route('/')
-def home():
-    return render_template('index.html')
+# 
+@app.route('/api')
+def main():
 
+    # TWITTER API CALL
+
+    sid = SentimentIntensityAnalyzer()
+    print(sid.polarity_scores('testing'))
+
+    # LOOP FOR 
+
+    return jsonify({"test":"testing"})
 
 
 if __name__ == '__main__':
-    ssl._create_default_https_context = _create_unverified_https_context
-    nltk.download('vader_lexicon')
-#    sid = SentimentIntensityAnalyzer()
-#    sid.polarity_scores(sentence)
+   # ssl._create_default_https_context = _create_unverified_https_context
+   # nltk.download('vader_lexicon')
+
     app.run(debug=False, use_reloader=False)
