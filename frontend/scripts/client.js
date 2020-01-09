9061ecc1352ff1d436279d2500ac81246633920a
Sahir Mody <sahir.mody@gmail.com>
2020-01-09 16:47:50 -0500

    add flask urls

---
+++ frontend/scripts/client.js
@@ -0,0 +1,24 @@
+
+/* Localhost for development */
+const URL = "http://localhost:5000"
+
+
+$.ajax(
+    {
+      url: URL.concat('api'),
+      type: 'GET',
+      success: function(result) {
+        
+      },
+      error: function(xhr,status,error) {
+        
+        // log the errors and alerts that an error has occurred
+        console.log(xhr);
+        console.log(status);
+        console.log(error);
+        alert("ERROR WITH API CALL! CHECK CONSOLE");
+  
+      }
+    }
+  );
+  
