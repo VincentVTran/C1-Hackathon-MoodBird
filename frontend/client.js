e78a087c35cccef6bdd9eea6fc159168fc1e40d4
Sahir Mody <sahir.mody@gmail.com>
2020-01-09 23:08:38 -0500

    fix url

--- frontend/client.js
+++ frontend/client.js
@@ -1,5 +1,5 @@
 
-const prodURL = 'http://vincentvtran.pythonanywhere.com/api/hello';
+const prodURL = 'http://vincentvtran.pythonanywhere.com/hello';
 const localURL = 'http://localhost:5000/api'
 
 
