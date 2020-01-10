204a47b3d06815c17f08badfae753d11acf77e41
Meico Hsiao <meicohsiao@gmail.com>
2020-01-09 21:54:45 -0500

    fixed some stuff

---
+++ frontend/graph.js
@@ -0,0 +1,6 @@
+var graph = document.getElementsByClassName('graph');
+
+
+document.getElementById('scatterPlot').onclick = function expandGraph() {
+    document.getElementById.style.width = "100px";
+}
