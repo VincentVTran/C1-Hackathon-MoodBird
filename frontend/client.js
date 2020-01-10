00b2ca642ada687b2efd720d2315c6a1d6ca6ad0
Vincent Tran <VincentTran197120@gmail.com>
2020-01-09 19:58:59 -0500

    Add client update

--- frontend/client.js
+++ frontend/client.js
@@ -8,6 +8,14 @@ function executeQuery() {
   setTimeout(executeQuery, 5000); // you could choose not to continue on failure...
 }
 
+$('#searchBar').keydown(function (e){
+  if(e.keyCode == 13){
+    var keyword = $("#searchBar").text()
+    element.innerHTML = element.innerText || element.textContent;
+    executeQuery(url+ document.getElementById('searchBar').innerHTML.replace(/<[^>]*>/g, ""));
+  }
+});
+
 $(document).ready(function() {
   // run the first time; all subsequent calls will take care of themselves
   setTimeout(executeQuery, 5000);
